941c4659fbed53f8f6f9b99fe04b99e9bb67d239
arundha-sara <149686095+arundha-sara@users.noreply.github.com>
2023-11-02 07:59:13 -0500

    Add files via upload

---
+++ ass1.js
@@ -0,0 +1,15 @@
+//1.create an empty array called fruits
+let fruits=[];
+// application of semicolon can be optional.
+//2. Add the following fruits to the "fruits" array: "apple", "banana", "orange".
+fruits= ['apple','banana','orange']
+console.log(fruits)
+//3. Remove the first fruit from the array.
+fruits.splice(0,1)
+console.log(fruits)
+//4.add grapes for the array.
+fruits.push('grapes')
+console.log(fruits)
+//5.upgrade the second fruit for peer.
+fruits[1]='peer'
+console.log(fruits)
